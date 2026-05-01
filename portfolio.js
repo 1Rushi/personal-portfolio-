fd5911ea0d8e86b921e3d0c0949a96994afc9a5e
1Rushi <kakaderushi85@gmail.com>
2026-05-01 17:33:43 +0530

    my portfolio project first commit

--- portfolio.js
+++ portfolio.js
@@ -12,3 +12,31 @@ const initSlider = () => {
 }
 
 window.addEventListener("load", initSlider);
+
+  function goToWork() {
+    window.location.href = "#project"; // change file   name as needed
+  }
+
+////////////project-button//////////////////////
+function openProject(url) {
+    window.open(url, "_blank");
+}
+
+////////////////////contact button////////////////
+function goContact(){
+    window.location.href = "contact.html";
+}
+
+
+function toggleText(el){
+    let parent = el.parentElement;
+    let moreText = parent.querySelector(".more-text");
+
+    if(moreText.style.display === "inline"){
+        moreText.style.display = "none";
+        el.innerText = " Read more";
+    } else {
+        moreText.style.display = "inline";
+        el.innerText = " Read less";
+    }
+}
